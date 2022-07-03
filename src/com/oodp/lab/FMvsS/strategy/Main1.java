ff930af9a265042104c63a43b2bd1fb2b17cd908
chp320 <chp320@naver.com>
2022-07-03 22:03:58 +0900

    strategy 패턴에 factory 추가 구현

--- src/com/oodp/lab/FMvsS/strategy/Main1.java
+++ src/com/oodp/lab/FMvsS/strategy/Main1.java
@@ -1,20 +1,22 @@
 package com.oodp.lab.FMvsS.strategy;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
 
+        StrategyFactory factory = new StrategyFactory();
         Account account1 = new Account(12345678l, "Vijay Kumar", AccountType.SAVING);
-        account1.setInterestStrategy(new CompoundInterestCalculator());
+        account1.setInterestStrategy(factory.createStrategy("COMPOUND"));
         account1.deposit(10000.0d);
 
         System.out.print(account1);
         System.out.println(" has interest : " + account1.getInterest(5));
 
         Account account2 = new Account(12345680l, "Jay Kumar", AccountType.SAVING);
-        account2.setInterestStrategy(new SimpleInterestCalculator());
+        account2.setInterestStrategy(factory.createStrategy("SIMPLE"));
         account2.deposit(10000.0d);
 
         System.out.print(account2);
         System.out.println(" has interest : " + account2.getInterest(5));
+
     }
 }
