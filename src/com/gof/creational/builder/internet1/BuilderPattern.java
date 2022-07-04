e433f98297862fa8f635cf031e848a3c008a8544
chp320 <chp320@naver.com>
2022-07-05 07:49:23 +0900

    BuilderPattern 구현

---
+++ src/com/gof/creational/builder/internet1/BuilderPattern.java
@@ -0,0 +1,14 @@
+package com.gof.creational.builder.internet1;
+
+public class BuilderPattern {
+    public static void main(String[] args) {
+        PersonInfoBuilder personInfoBuilder = new PersonInfoBuilder();
+        // 빌더 패턴을 통해 순서에 상관없이 객체 생성 가능하고, 사용자 관점에서 명시적으로 이해 가능
+        PersonInfo result = personInfoBuilder
+                .setName("MISTAKE")
+                .setFavoriteAnimal("cat")
+                .setFavoriteColor("black")
+                .build() ;
+        System.out.println(result.getPersonInfo());
+    }
+}
