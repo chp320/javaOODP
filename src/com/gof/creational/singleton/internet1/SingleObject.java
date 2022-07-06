c51586dc1bf6655cb050bbeda1b5f4eb15a56ca8
chp320 <chp320@naver.com>
2022-07-07 07:41:30 +0900

    main 내 싱글톤 호출 부분 수정

--- src/com/gof/creational/singleton/internet1/SingleObject.java
+++ src/com/gof/creational/singleton/internet1/SingleObject.java
@@ -2,8 +2,8 @@ package com.gof.creational.singleton.internet1;
 
 public class SingleObject {
 
-    // create an object of SingleObject
-    private static SingleObject instance = new SingleObject();
+    // create an object of SingleObject (final 키워드를 사용해서 다시 값이 할당 되는 것을 방지, multi-thread 환경..)
+    private static final SingleObject instance = new SingleObject();
 
     // make constructor private so that this class cannot be instantiated
     private SingleObject(){
--- src/com/gof/creational/singleton/internet1/SingletonPatternDemo.java
+++ src/com/gof/creational/singleton/internet1/SingletonPatternDemo.java
@@ -4,9 +4,13 @@ public class SingletonPatternDemo {
     public static void main(String[] args) {
 
         // get the only object available
-        SingleObject object = SingleObject.getInstance();
+        SingleObject object1 = SingleObject.getInstance();
+        SingleObject object2 = SingleObject.getInstance();
+        SingleObject object3 = SingleObject.getInstance();
 
-        object.showMessage();
 
+        System.out.println(object1.hashCode());
+        System.out.println(object2.hashCode());
+        System.out.println(object3.hashCode());
     }
 }
