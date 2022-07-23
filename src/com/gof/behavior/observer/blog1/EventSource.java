1e87641c35f490357362c621a1ad8f6213caaefa
chp320 <chp320@naver.com>
2022-07-23 22:40:43 +0900

    Observer 패턴 내 주석 추가

--- src/com/gof/behavior/observer/blog1/EventSource.java
+++ src/com/gof/behavior/observer/blog1/EventSource.java
@@ -10,8 +10,10 @@ import java.util.Observable;    /* 옵저버에게 신호를 보내는 주체 */
  * - 옵저버 패턴은 객체의 상태 변화를 관찰하는 '옵저버'의 목록을 객체에 등록 후 변화가 있을 때마다
  *   메서드 등을 통해 객체가 직접 목록의 각 옵저버에게 통지하도록 하는 디자인 패턴.
  * - 주로 분산 이벤트 핸들링 시스템을 구현하는데 사용. 발행/구독 모델로 알려져 있음.
+ * - 본 예시는 위키피디아에 등록된 예시
  *
  * 참고) https://m.blog.naver.com/dktmrorl/222091486735
+ * 참고) https://ko.wikipedia.org/wiki/%EC%98%B5%EC%84%9C%EB%B2%84_%ED%8C%A8%ED%84%B4
  */
 public class EventSource extends Observable implements Runnable {
     @Override
