81306479d0fdd8fa6c416f6a583df0db5e2a2ea4
chp320 <chp320@naver.com>
2022-07-31 22:03:08 +0900

    세미나 발표자료 최종 정리

--- src/com/gof/behavior/state/internet2/ModeSwitch.java
+++ src/com/gof/behavior/state/internet2/ModeSwitch.java
@@ -2,6 +2,7 @@ package com.gof.behavior.state.internet2;
 
 /**
  * Context - 상태(on/off)를 갖는 객체와 실행하는 클라이언트 구현
+ * => 상태를 관리하는 State 객체를 처음 Dark 모드(StateDark)로 설정
  */
 public class ModeSwitch {
     private State modeState = new StateDark();
